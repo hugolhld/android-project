e8fd5453f8d04453349ed728957b1cfe3715bd69
hugolhld <hugo.lhernould@gmail.com>
2020-06-10 22:50:31 +0200

    🛠 Add search city and the weather for thr 4 next hours

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,36 +1,123 @@
 package com.androdocs.weatherapp
 
+import android.annotation.SuppressLint
 import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.androdocs.weatherapp.R.*
+import com.androdocs.weatherapp.R.id.*
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
 
     val API: String = "167d58d024f7bab696b3bb1dea0260ca"
-    val CITY: String = "Paris"
+    var CITY: String = "Paris"
+    var lon: String = "2.35"
+    var lat: String = "48.85"
+
     /*val content: String = search_city.getText().toString()*/
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+
+//        val test = "test"
+//
+//        second_hour.text = test
+
+        weatherHourly().execute()
+
         weatherTask().execute()
 
+        button_search.setOnClickListener {
+            val searchCity: String = search_city.text.toString()
+
+            CITY = searchCity
+
+            weatherTask().execute()
+            weatherHourly().execute()
+
+        }
+
+    }
+
+    inner class weatherHourly(): AsyncTask<String, Void, String>(){
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response:String?
+
+            try {
+                response = URL("https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=minutely&units=metric&appid=$API").readText(
+                    Charsets.UTF_8
+                )
+
+
+            }catch (e: Exception){
+                response = null
+            }
+
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                val jsonObj = JSONObject(result)
+                val hourlyTab = jsonObj.getJSONArray("hourly")
+
+                val firstHourJSON = hourlyTab.getJSONObject(1).getLong("dt")
+                val firstHour = SimpleDateFormat("HH", Locale.FRANCE).format(Date(firstHourJSON * 1000)) + "h"
+                first_hour.text = firstHour
+
+                val firstWeather = hourlyTab.getJSONObject(1).getString("temp")
+                first_weather.text = firstWeather.toDouble().toInt().toString() + "°"
+
+                val secondtHourJSON = hourlyTab.getJSONObject(2).getLong("dt")
+                val secondHour = SimpleDateFormat("HH", Locale.FRANCE).format(Date(secondtHourJSON * 1000)) + "h"
+                second_hour.text = secondHour
+
+                val secondWeather = hourlyTab.getJSONObject(2).getString("temp")
+                second_weather.text = secondWeather.toDouble().toInt().toString() + "°"
+
+                val thirdtHourJSON = hourlyTab.getJSONObject(3).getLong("dt")
+                val thirdHour = SimpleDateFormat("HH", Locale.FRANCE).format(Date(thirdtHourJSON * 1000)) + "h"
+                third_hour.text = thirdHour
+
+                val thirdWeather = hourlyTab.getJSONObject(3).getString("temp")
+                third_weather.text = thirdWeather.toDouble().toInt().toString() + "°"
+
+                val fourHourJSON = hourlyTab.getJSONObject(4).getLong("dt")
+                val fourHour = SimpleDateFormat("HH", Locale.FRANCE).format(Date(fourHourJSON * 1000)) + "h"
+                four_hour.text = fourHour
+
+                val fourWeather = hourlyTab.getJSONObject(4).getString("temp")
+                four_weather.text = fourWeather.toDouble().toInt().toString() + "°"
+
+
+            } catch (e: Exception) {
+                Log.d("error", e.toString())
+                findViewById<ProgressBar>(id.loader).visibility = View.GONE
+            }
+        }
     }
 
     inner class weatherTask() : AsyncTask<String, Void, String>() {
 
         override fun doInBackground(vararg params: String?): String? {
+
             var response:String?
             try{
                 response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
@@ -49,6 +136,9 @@ class MainActivity : AppCompatActivity() {
                 val jsonObj = JSONObject(result)
                 val main = jsonObj.getJSONObject("main")
                 val sys = jsonObj.getJSONObject("sys")
+                val coord = jsonObj.getJSONObject("coord")
+                lon = coord.getString("lon")
+                lat = coord.getString("lat")
                 val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
 
                 val updatedAt:Long = jsonObj.getLong("dt")
@@ -58,20 +148,19 @@ class MainActivity : AppCompatActivity() {
 
                 val address = jsonObj.getString("name")+", "+sys.getString("country")
 
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.updated_at).text =  updatedAtText
-                findViewById<TextView>(R.id.temp).text = temp
+                findViewById<TextView>(id.address).text = address
+                findViewById<TextView>(id.updated_at).text =  updatedAtText
+                findViewById<TextView>(id.temp).text = temp
 
 
-                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
-                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
-
-
+                findViewById<ProgressBar>(id.loader).visibility = View.GONE
+                //findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
 
             } catch (e: Exception) {
-                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<ProgressBar>(id.loader).visibility = View.GONE
             }
 
         }
+
     }
 }
